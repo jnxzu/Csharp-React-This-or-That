fbf04074a86d08a758390f2230c4dcaebac43793
J <janbielowka@gmail.com>
2020-09-11 21:19:55 +0200

    mainpage

---
+++ ClientApp/src/components/MainPage/ItemCard/ItemCard.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ItemCard extends Component {
+  render() {
+    return <div></div>;
+  }
+}
--- ClientApp/src/components/MainPage/MainPage.js
+++ ClientApp/src/components/MainPage/MainPage.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
+import ItemCard from "./ItemCard/ItemCard";
+
+import "./mainpage.scss";
+
 export default class MainPage extends Component {
   render() {
-    return <></>;
+    return (
+      <div className="main">
+        <ItemCard />
+        <div className="main__vs">vs</div>
+        <ItemCard />
+      </div>
+    );
   }
 }
