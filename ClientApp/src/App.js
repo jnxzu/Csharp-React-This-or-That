b1d534dd1820bd5a705b7f0591ccc055716fcafc
J <janbielowka@gmail.com>
2020-09-11 19:10:59 +0200

    setup, created nav

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Nav from "./components/Nav/Nav";
 import MainPage from "./components/MainPage/MainPage";
 import AdminPage from "./components/AdminPage/AdminPage";
 import SubmitPage from "./components/SubmitPage/SubmitPage";
 
-import "./App.css";
+import "./app.scss";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div className="app">
-          <Header />
+          <Nav />
           <Switch>
             <Route exact path="/" component={MainPage} />
             <Route path="/admin" component={AdminPage} />
             <Route path="/submit" component={SubmitPage} />
           </Switch>
-          <Footer />
         </div>
       </Router>
     );
